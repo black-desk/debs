e68e15bf10710939dad29fd85776414b328ad578
black-desk <me@black-desk.cn>
2024-02-07 10:54:21 +0800

    fix: add profile.d

---
+++ etc/profile.d/60-io.neovim.neovim-nightly.sh
@@ -0,0 +1,6 @@
+if [ -f /etc/environment.d/60-io.neovim.neovim-nightly.conf ]; then
+	source /etc/environment.d/60-io.neovim.neovim-nightly.conf
+fi
+export PATH
+export MANPATH
+export XDG_DATA_DIRS
