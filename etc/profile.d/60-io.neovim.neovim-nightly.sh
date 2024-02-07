5253a3032fdbbed4dbf900ab81d73af626209adb
black-desk <me@black-desk.cn>
2024-02-07 11:28:33 +0800

    fix: only export PATH

--- etc/profile.d/60-io.neovim.neovim-nightly.sh
+++ etc/profile.d/60-io.neovim.neovim-nightly.sh
@@ -1,6 +1,8 @@
 if [ -f /etc/environment.d/60-io.neovim.neovim-nightly.conf ]; then
 	source /etc/environment.d/60-io.neovim.neovim-nightly.conf
 fi
+
+# NOTE:
+# We only need to export PATH here.
+# As PATH is the only one which is overwrited by /etc/profile.
 export PATH
-export MANPATH
-export XDG_DATA_DIRS
