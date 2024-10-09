e66f5586c7f7ae18ffff1f95c5684499be73a692
black-desk <me@black-desk.cn>
2024-10-09 19:06:29 +0800

    feat: generate Packages file

--- release.sh
+++ release.sh
@@ -3,5 +3,7 @@
 set -x
 set -e
 
+dpkg-scanpackages -m . /dev/null >Packages
+
 (gh release list --json tagName | jq .\[\].tagName -r | xargs -L1 gh release delete) || true
-gh release create --latest "$(date +%s)" *.deb
+gh release create --latest "$(date +%s)" ./*.deb ./Packages
