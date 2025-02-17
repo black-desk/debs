88ac66c5c550a072d7d28f0dd877c8c3e553ed4d
black-desk <me@black-desk.cn>
2025-02-17 16:47:58 +0800

    Revert "build: try fix distcc"
    This reverts commit 0be7506a54b79d5af145b91c2c77eedae423e665.

--- build.sh
+++ build.sh
@@ -6,12 +6,6 @@ set -x
 sudo apt update
 sudo apt install -y devscripts equivs build-essential
 
-pushd ./distcc
-mk-build-deps
-sudo apt install -y -- ./*.deb
-dpkg-buildpackage -us -uc -b
-popd
-
 for dir in */; do
 	pushd "$dir"
 	if [ -f ./tools/buildpackage.sh ]; then
